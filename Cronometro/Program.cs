3fe9c2057b1b7b45481d7801b81b3f79c1102bd0
dev81log <dev81log@gmail.com>
2022-05-17 20:42:29 -0300

    git inicial programa

---
+++ Cronometro/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Cronometro
+{
+    static class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S - Segundos");
+            Console.WriteLine("M - Minutos");
+            Console.WriteLine("0 - Parar Cronometro");
+            Console.WriteLine("Quanto tempo deseja cronometrar?");
+
+        }
+
+        static void Start(int tempo)
+        {
+            int tempoAtual = 0;
+
+            while (tempoAtual != tempo)
+            {
+                tempoAtual++;
+                Console.Clear();
+                Console.WriteLine($"Tempo atual {tempoAtual}");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Tempo finalizado");
+            Thread.Sleep(2000);
+
+        }
+    }
+
+}
